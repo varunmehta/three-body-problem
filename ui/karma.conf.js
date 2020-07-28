fd8fef51917bb562047954c78146d55b10af29b9
dami.okuneye <Expand2020>
2020-07-27 22:53:47 -0400

    Added contract tests

--- ui/karma.conf.js
+++ ui/karma.conf.js
@@ -7,13 +7,17 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
+      require('@pact-foundation/karma-pact'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+        timeoutInterval: 40000
+      }
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/ui'),
@@ -26,7 +30,19 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    singleRun: true,
+    restartOnFileChange: true,
+    pact: [
+      {
+        cors: true,
+        spec: 3,
+        port: 1234,
+        log: path.resolve(process.cwd(), 'logs', 'mockserver-integration.log'),
+        dir: path.resolve(process.cwd(), '../../pacts')
+      }
+    ],
+    proxies: {
+      '/api': 'http://localhost:1234/api'
+    }
   });
 };

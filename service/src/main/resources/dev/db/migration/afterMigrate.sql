d9c0b8c2d3bf296705c3fba9ee3d6bd8833fb488
Varun Mehta <varun.mehta@S00405-MBPR.local>
2020-08-24 14:22:31 -0400

    Made the sample dev data script idempotent

--- service/src/main/resources/dev/db/migration/afterMigrate.sql
+++ service/src/main/resources/dev/db/migration/afterMigrate.sql
@@ -1,6 +1,12 @@
-INSERT INTO customer (first_name, last_name, email, funky_id, created_on, updated_on) VALUES ('Clark','Kent','superman@jl.com','superman',current_timestamp,current_timestamp);
-INSERT INTO customer (first_name, last_name, email, funky_id, created_on, updated_on) VALUES ('Bruce','Wayne','batmam@jl.com','batman',current_timestamp,current_timestamp);
-INSERT INTO customer (first_name, last_name, email, funky_id, created_on, updated_on) VALUES ('Diana','Prince','ww@jl.com','wonder_woman',current_timestamp,current_timestamp);
-INSERT INTO customer (first_name, last_name, email, funky_id, created_on, updated_on) VALUES ('Barry','Allen','flash@jl.com','flash',current_timestamp,current_timestamp);
-INSERT INTO customer (first_name, last_name, email, funky_id, created_on, updated_on) VALUES ('Hal','Jordan','gl@jl.com','green_lantern',current_timestamp,current_timestamp);
-INSERT INTO customer (first_name, last_name, email, funky_id, created_on, updated_on) VALUES ('Arthur','Curry','aquaman@jl.com','aquaman',current_timestamp,current_timestamp);
+INSERT INTO customer (first_name, last_name, email, funky_id, created_on, updated_on)
+SELECT first_name, last_name, email, funky_id, current_timestamp, current_timestamp
+FROM (
+  VALUES
+    ('Clark','Kent','superman@jl.com','superman'),
+    ('Bruce','Wayne','batmam@jl.com','batman'),
+    ('Diana','Prince','ww@jl.com','wonder_woman'),
+    ('Barry','Allen','flash@jl.com','flash'),
+    ('Hal','Jordan','gl@jl.com','green_lantern'),
+    ('Arthur','Curry','aquaman@jl.com','aquaman')
+) AS data(first_name, last_name, email, funky_id)
+WHERE NOT EXISTS (SELECT first_name, last_name, email, funky_id FROM customer WHERE data = customer);
